afca52e3acc3a19f2b8a475ee9c58ba97c7b1c46
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-05-17 21:55:55 +0900

    initial commit

---
+++ app.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+require 'ramaze'
+require 'datamapper'
+
+# モデル
+DataMapper.setup(:default, "sqlite3://#{__DIR__('words.db')}")
+class Word
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :description, Text
+
+  validates_is_unique :name
+end
+DataMapper.auto_upgrade!
+
+# コントローラ
+class Words < Ramaze::Controller
+  map '/'
+  engine :ERB
+  layout 'default'
+
+  def index
+    @words = Word.all
+  end
+
+  def new
+  end
+
+  def create
+    word = Word.create({
+      :name => request['name'],
+      :description => request['description']
+    })
+
+    if word
+      redirect r(:show, word.id)
+    else
+      redirect_referer
+    end
+  end
+
+  def show(id)
+    @word = Word.get(id)
+  end
+
+  def random
+    at = rand(Word.count)
+    @word = Word.first(:offset => at)
+
+    render_view :show
+  end
+end
+
+Ramaze.start :port => 7999
